505c6f5334aeb55c7021b66edf0f5ed9341d0caf
hw16a051 <hw16a051@oecu.jp>
2017-11-14 16:06:03 +0900

    作業(F)の実装をした

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -43,7 +43,7 @@ void Update()
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
         if (targetRect.Overlaps(bulletRect)) {
-            score += 1;         // スコアの加算
+            score += 100;         // スコアの加算//（実装：HW16A051 上手一貴）
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
     }
@@ -68,8 +68,8 @@ void Update()
     FillRect(targetRect, Color::red);
 
     // スコアの描画
-    SetFont("nicoca_v1.ttf", 80.0f);
-    DrawText(FormatString("%02d", score), Vector2(-319, 149), Color::black);
-    DrawText(FormatString("%02d", score), Vector2(-320, 150), Color::white);
+    SetFont("nicoca_v1.ttf", 80.0f);//（実装：HW16A051 上手一貴）
+    DrawText(FormatString("%05d", score), Vector2(-319, 149), Color::black);//（実装：HW16A051 上手一貴）
+    DrawText(FormatString("%05d", score), Vector2(-320, 150), Color::white);//（実装：HW16A051 上手一貴）
 }
 
